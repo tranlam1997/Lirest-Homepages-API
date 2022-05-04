b114f5e854df4b2982abc4e810ed52ed64701e9e
Travis <travis.trangbui@setel.com>
2022-05-04 10:54:25 +0700

    update husky config

--- src/global/winston-config.ts
+++ src/global/winston-config.ts
@@ -1,19 +1,20 @@
 import winston, { LoggerOptions } from 'winston';
 
-const {combine, timestamp, printf, json,} = winston.format;
-const formatInfo = printf(({level, message, timestamp}) => { return `level: ${level}, timestamp: ${timestamp}, message: ${message}`; });
+const { combine, timestamp, printf, json } = winston.format;
+const formatInfo = printf(
+  ({ level, message, timestamp }: { level: string; message: string; timestamp: string }) => {
+    return `level: ${level}, timestamp: ${timestamp}, message: ${message}`;
+  },
+);
 export default winston.createLogger({
-    format: combine(
-        timestamp(),
-        formatInfo,
-        json(),
-    ),
-    transports: [
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            handleRejections: true,
-        }),
-    ],
-    exitOnError: false
-} as LoggerOptions)
+  format: combine(timestamp(), formatInfo, json()),
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      handleRejections: true,
+    }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+  exitOnError: false,
+} as LoggerOptions);
